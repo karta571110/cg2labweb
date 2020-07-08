5672997c9926a980787b9716a246af94bd2ebf18
WMC <karta571110@gmail.com>
2020-07-08 19:25:52 +0800

    UserNameCheck

--- Service/ContextFactory.cs
+++ Service/ContextFactory.cs
@@ -12,7 +12,7 @@ namespace Service
         public ApplicationDbContext dbContext()
         {
             var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
-            optionsBuilder.UseMySQL("server=192.168.1.68;database=cg2lab;user=root;password=cg2lab;SslMode=none");
+            optionsBuilder.UseMySQL("server=192.168.1.68;database=cg2lab;user=cg2lab;password=cg2lab;SslMode=none");
             return new ApplicationDbContext(optionsBuilder.Options); 
         }
         public ApplicationDbContext CreateDbContext(string[] args)
